ffac670ae2bed55b25fc6c19b7304d974da18833
tahmonabot-eng <tahmonabot@gmail.com>
2026-04-15 17:44:01 +0600

    Update autoseen.js

--- scripts/cmds/autoseen.js
+++ scripts/cmds/autoseen.js
@@ -10,13 +10,27 @@ module.exports.config = {
   shortDescription: "DM ও Group-এ message automatic seen করে"
 };
 
+module.exports.config = {
+  name: "autoseen",
+  version: "3.1.0",
+  role: 0,
+  credits: "Ariful Islam Sabbir",
+  hidden: true,
+  usePrefix: false,
+  category: "System",
+  countDown: 0,
+  shortDescription: "DM ও Group-এ message automatic seen করে"
+};
+
 module.exports.onStart = async function () {};
 
 module.exports.onChat = async function ({ api, event }) {
-  try {
-    await api.markAsSeen(Date.now());
-    await api.markAsRead(event.threadID, true);
-  } catch (e) {
-    // silent
+  if (event.body) {
+    try {
+      // এটি মেসেজটিকে সিন (Seen) হিসেবে মার্ক করবে
+      await api.markAsRead(event.threadID);
+    } catch (e) {
+      // console.log(e);
+    }
   }
 };
