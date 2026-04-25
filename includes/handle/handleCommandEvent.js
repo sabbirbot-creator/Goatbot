c794ca122a93b46fd0467b6c857c8454be96957e
sabbirbot-creator <sabbirmdarifulislam@gmail.com>
2026-04-26 01:19:33 +0600

    Update handleCommandEvent.js

--- includes/handle/handleCommandEvent.js
+++ includes/handle/handleCommandEvent.js
@@ -1,45 +1,62 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
     const logger = require("../../utils/log.js");
-    return function ({ event }) {
-        const { allowInbox } = global.config;
+
+    return async function ({ event }) {
+
         const { userBanned, threadBanned } = global.data;
         const { commands, eventRegistered } = global.client;
-        var { senderID, threadID } = event;
-        senderID = String(senderID);
-        threadID = String(threadID);
-        if (userBanned.has(senderID) || threadBanned.has(threadID) || (allowInbox == false && senderID == threadID)) return;
-        for (const eventReg of eventRegistered) {
-            const cmd = commands.get(eventReg);
-            var getText2;
 
-            if (cmd.languages && typeof cmd.languages == 'object') {
-                getText2 = (...values) => {
-                    const commandModule = cmd.languages || {};
-                    if (!commandModule.hasOwnProperty(global.config.language)) 
-                        return api.sendMessage(global.getText('handleCommand','notFoundLanguage', cmd.config.name), threadID, messengeID); 
-                    var lang = cmd.languages[global.config.language][values[0]] || '';
-                    for (var i = values.length - 1; i >= 0; i--) {
-                        const expReg = RegExp('%' + (i + 1), 'g');
-                        lang = lang.replace(expReg, values[i]);
-                    }
-                    return lang;
-                };
-            } else {
-                getText2 = () => {};
-            }
+        if (!event || !event.threadID || !event.senderID) return;
+
+        const senderID = String(event.senderID);
+        const threadID = String(event.threadID);
+
+        const allowInbox = global.config?.allowInbox;
+
+        // SAFE FILTER (FIXED)
+        if (userBanned.has(senderID)) return;
+        if (threadBanned.has(threadID)) return;
+
+        if (allowInbox === false && senderID === threadID) return;
+
+        for (const eventReg of eventRegistered || []) {
+
+            const cmd = commands.get(eventReg);
+            if (!cmd) continue;
+
+            let getText2 = () => {};
 
             try {
-                const Obj = {};
-                Obj.event = event;
-                Obj.api = api;
-                Obj.models = models;
-                Obj.Users = Users;
-                Obj.Threads = Threads;
-                Obj.Currencies = Currencies;
-                Obj.getText = getText2;
-                if (cmd) cmd.handleEvent(Obj);
+                if (cmd.languages?.[global.config.language]) {
+                    getText2 = (...values) => {
+                        let lang = cmd.languages[global.config.language][values[0]] || '';
+                        for (let i = values.length - 1; i >= 0; i--) {
+                            lang = lang.replace(new RegExp('%' + (i + 1), 'g'), values[i]);
+                        }
+                        return lang;
+                    };
+                }
+
+                const Obj = {
+                    event,
+                    api,
+                    models,
+                    Users,
+                    Threads,
+                    Currencies,
+                    getText: getText2
+                };
+
+                // IMPORTANT: await added
+                if (typeof cmd.handleEvent === "function") {
+                    await cmd.handleEvent(Obj);
+                }
+
             } catch (error) {
-                logger(global.getText('handleCommandEvent', 'moduleError', cmd.config.name), 'error');
+                logger(
+                    `Event Error in ${cmd?.config?.name || "unknown"}: ${error.message}`,
+                    "error"
+                );
             }
         }
     };
