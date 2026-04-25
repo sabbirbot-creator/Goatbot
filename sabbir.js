4db8fcb08066b3874763e66ad61ca119026032fb
sabbirbot-creator <sabbirmdarifulislam@gmail.com>
2026-04-25 16:15:21 +0600

    Update sabbir.js

--- sabbir.js
+++ sabbir.js
@@ -13,10 +13,10 @@ const logHandler = require("./utils/Log.js");
 process.env.BLUEBIRD_W_FORGOTTEN_RETURN = 0;
 
 const dirConfig = path.normalize(`${__dirname}/config.json`);
-const dirConfigCommands = path.normalize(`${__dirname}/configCommands.json`);
+//const dirConfigCommands = path.normalize(`${__dirname}/configCommands.json`);
 const dirAccount = path.normalize(`${__dirname}/account.txt`);
 
-//const config = require(dirConfig);
+const config = require(dirConfig);
 //const configCommands = require(dirConfigCommands);
 
 // Map alternate field names used by original Goat-Bot-V2
